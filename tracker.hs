fc8a378d699ed4151567e93bc81ab784205c9fb9
mgenius <mgenius@mgenius-VAIO.(none)>
2013-04-07 22:28:16 +0530

    tracker_2

--- tracker.hs
+++ tracker.hs
@@ -1,12 +1,43 @@
 import Network.Info
+import Network.Socket
+import Control.Concurrent
+import Control.Exception
+import Network
+import System.IO
 
-main = do
-         ni <- getNetworkInterfaces
-         putStr ( show (checkinterface ni ) )
+--putStr ( show (checkinterface ni ) )
+
+getIpEth0 (n:ns) = if ((name n) == "eth0")
+                            then show (ipv4 n)
+                          else getIpEth0 ns
+
+getIpEth0 [] = "127.0.0.1"
+
+ipEth :: IO HostAddress
+ipEth = do
+	ni <- getNetworkInterfaces
+	inet_addr(getIpEth0 ni)
 
 
-checkinterface (n:ns) = if ((name n) == "eth0")
-                            then Just ( show (ipv4 n) )
-                          else checkinterface ns
+listen_port = 10116
+message = "hello, world"
 
-checkinterface [] = Nothing
+main = do 
+	sock  <- socket AF_INET Stream defaultProtocol
+	haddr <- ipEth
+	bindSocket sock (SockAddrInet listen_port haddr)
+	acceptLoop sock `finally` sClose sock
+
+	
+	--putStr haddr
+
+acceptLoop :: Socket -> IO ()
+acceptLoop sock = forever $ accept sock >>= forkIO . worker
+
+worker :: (Handle, HostName, PortNumber) -> IO ()
+worker (hand, host, port ) = do
+		
+
+	
+
+
