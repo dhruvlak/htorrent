3e4403f1fe3fd57a2b4174b63abf52c2405ffb67
peeks <mgenius@mgenius-VAIO.(none)>
2013-04-12 20:08:31 +0530

    tracker updating file list

--- client.hs
+++ client.hs
@@ -29,38 +29,76 @@ mainLoop :: ClientM ()
 mainLoop = do  
 	doIO $ putStrLn "htorrent :"
 	s <- doIO getLine
+	clientSock <- doIO (socket AF_INET Stream defaultProtocol)
+	
 	interpretResult <- doIO $ interpret s
-	if (interpretResult == 1) then
-		do
-			x <- doIO $ connectToTracker
-			put x
-	else
+	if (interpretResult == 0) then
 		doIO $ putStrLn "Unknown input"
-	currentState <- get
-	doIO $ print ("current state:"++ (show currentState))
+	else
+		if (interpretResult ==1 || interpretResult == 2 || interpretResult ==3)	then 
+			do
+				env <- get
+				x <- doIO (connectToTracker interpretResult env clientSock)
+				put x
+		else
+			doIO $ putStrLn "we are working on this option"
+	
+	newEnv <- get
+	doIO $ print ("current state:"++ (show newEnv))
 	mainLoop
 
 interpret :: String -> IO Int
 interpret s = if s == "connect" then
 			return 1
 		else
-		do
-			putStrLn "Wrong Input" 
-			return 0
+		 	if s == "seed" then
+				return 2 
+			
+			else
+			  do
+				putStrLn "Wrong Input" 
+				return 0
 	
-connectToTracker::IO Env
-connectToTracker = do
+connectToTracker::Int ->Env-> Socket ->IO Env
+connectToTracker requestType env clientSock= 
 		--putStrLn s 
-		trackerAddr <- inet_addr tracker_ip
-		client_sock <- socket AF_INET Stream defaultProtocol
-		connect client_sock (SockAddrInet tracker_port trackerAddr)
-		getEnv client_sock ""
-
+		if (requestType == 1) then
+		 do
+			trackerAddr <- inet_addr tracker_ip
+			connect clientSock (SockAddrInet tracker_port trackerAddr)
+			getEnv clientSock ""
+			
+		else if( requestType ==2) then
+		 do
+			trackerAddr <- inet_addr tracker_ip
+			putStrLn "enter file name:"
+			fileName <- getLine
+			connect clientSock (SockAddrInet tracker_port trackerAddr)
+			updateTrackerEnv clientSock fileName
+			return env
+			
+		      else
+				return env
+			
 getEnv :: Socket -> String -> IO Env
-getEnv clientSock msgReceived = do
+getEnv clientSock msgReceived =do
+				trackerAddr <- inet_addr tracker_ip
+ 			        sendTo clientSock "$getEnv$" (SockAddrInet tracker_port trackerAddr) 
+				getEnvLoop clientSock msgReceived			
+	
+getEnvLoop :: Socket -> String -> IO Env
+getEnvLoop clientSock msgReceived = do
+					--trackerAddr <- inet_addr tracker_ip
+					--sendTo clientSock "$getEnv$" (SockAddrInet tracker_port trackerAddr) 
 					m <- (recv clientSock 100)
 					print m
 					if ( last m == '$' ) then
-						return (read (msgReceived ++ (filter (\x -> x/='$') m)))
-					else getEnv clientSock (msgReceived++ m) 
+						do
+							sClose clientSock
+							return (read (msgReceived ++ (filter (\x -> x/='$') m)))
+					else getEnvLoop clientSock (msgReceived++ m) 
+
+updateTrackerEnv clientSock fileName = do	
+					trackerAddr <- inet_addr tracker_ip
+					sendTo clientSock fileName (SockAddrInet tracker_port trackerAddr) 
 					
--- tracker.hs
+++ tracker.hs
@@ -29,45 +29,57 @@ ipEth = do
 	inet_addr (getIpEth0 ni)
 	
 
-listen_port = 10116
+listenPort = 10116
+
 message = "hello, world"
 
 main = do 
 	sock  <- socket AF_INET Stream defaultProtocol
 	haddr <- ipEth
-	bindSocket sock (SockAddrInet listen_port haddr)
+	bindSocket sock (SockAddrInet listenPort haddr)
 	acceptLoop sock `finally` sClose sock
 
 	--putStr haddr
 
 acceptLoop :: Socket -> IO ()
 acceptLoop sock = do
-			fromCli <- newEmptyMVar
+			mvarFile <- newEmptyMVar
+			mvarSeeder <- newEmptyMVar
 			mvarEnv <- newEmptyMVar
-			maintainEnvTid <- (forkIO (maintainEnv fromCli mvarEnv))
+			maintainEnvTid <- (forkIO (maintainEnv mvarFile mvarSeeder mvarEnv))
 			--checkPeersTid <- (forkIO (checkPeers updatedEnv))
 			listen sock 2
 			forever (do
 					accepted_sock <- accept sock 
-					forkIO (worker accepted_sock fromCli mvarEnv)
+					forkIO (worker sock accepted_sock mvarFile mvarSeeder mvarEnv)
 				)
 
-worker :: (Socket,SockAddr) -> MVar String ->MVar Env -> IO ()
-worker (sock, (SockAddrInet pn ha) ) m u = 
+worker :: Socket -> (Socket,SockAddr) -> MVar String->MVar String ->MVar Env -> IO ()
+worker serverSock (clientSock, (SockAddrInet pn ha) ) mvarFile mvarSeeder mvarEnv = 
 	do
-		ha_1 <- inet_ntoa ha
-		print ha_1
+		clientIP <- inet_ntoa ha
+		--print ha_1
+		requestType <- (recv clientSock 100)
 		--sClose sock
-		tryTakeMVar u
-		putMVar m (ha_1 ++ ":" ++ show(pn))
+		if (requestType == "$getEnv$") then
+		   do	
+			tryTakeMVar mvarEnv
+			putMVar mvarFile ("")
+			putMVar mvarSeeder ("")
 		--takeMVar u
 		--tryTakeMVar u
-		newEnv <- takeMVar u
-		sendNum <- send sock ((show (newEnv)) ++ "$")
+			newEnv <- takeMVar mvarEnv
+			sendNum <- send clientSock ((show (newEnv)) ++ "$")
 		--send sock "$#finish#$"
-		sClose sock
-		print ("send:"++ show (sendNum))
-
+			sClose clientSock
+			print ("send:"++ show (sendNum))
+		else 
+		  do
+			tryTakeMVar mvarEnv
+			putMVar mvarFile requestType
+			putMVar mvarSeeder clientIP
+			print ("updating:")
+			sClose clientSock
 
 ------------------------------------------------------------------------
 
@@ -75,29 +87,44 @@ worker (sock, (SockAddrInet pn ha) ) m u =
 
 ------------------------------------------------------------------------
 
-maintainEnv :: MVar String -> MVar Env ->IO ()
-maintainEnv m u = runTrackerM (updateListLoop m u)
+maintainEnv :: MVar String -> MVar String-> MVar Env ->IO ()
+maintainEnv mvarFile mvarSeeder mvarEnv = runTrackerM (updateListLoop mvarFile mvarSeeder mvarEnv)
 
-updateListLoop :: MVar String -> MVar Env -> TrackerM ()
-updateListLoop m u = do 
-			s <- doIO (takeMVar m)
-			x <- (updateList s u)
-			updateListLoop m u
+updateListLoop :: MVar String-> MVar String -> MVar Env -> TrackerM ()
+updateListLoop mvarFile mvarSeeder mvarEnv = do 
+			fileName <- doIO (takeMVar mvarFile)
+			seeder <- doIO (takeMVar mvarSeeder)
+			x <- (updateList fileName seeder mvarEnv)
+			updateListLoop mvarFile mvarSeeder mvarEnv
 			
 
-updateList :: String ->MVar Env-> TrackerM ()
-updateList a mvarEnv = do 
-		x <- get
-		put (modifiedEnv x)
-		doIO (print (modifiedEnv x))
+updateList :: String -> String ->MVar Env-> TrackerM ()
+updateList fileName seeder mvarEnv = do 
+		env <- get
+		put (modifiedEnv env fileName seeder)
+		doIO (print (modifiedEnv env fileName seeder))
 		doIO ( forkIO (do  v <- tryTakeMVar mvarEnv
-				   putMVar mvarEnv (modifiedEnv x)))
+				   putMVar mvarEnv (modifiedEnv env fileName seeder)
+			      )
+		     )
 		return ()
 		
-		where modifiedEnv s = (Env {connectedPeers = (a:(connectedPeers s)), files = (files s)} )
+		where modifiedEnv env fileName seeder =  Env {connectedPeers = (connectedPeers env), files = (modifyFileList (files env) fileName seeder)} 
 
 
+modifyFileList::[(String,[String])] -> String -> String -> [(String,[String])]
+modifyFileList (x:xs) fileName seeder = if (fst x) == fileName then
+						((fileName,(addUnique seeder (snd x))):xs)
+					else (x:(modifyFileList xs fileName seeder))
+modifyFileList [] fileName seeder = [(fileName,[seeder])]
 --ListM a = StateT [String] IO a	
+
+addUnique :: String ->[String] -> [String]
+addUnique s (x:xs) = if (x==s) then (x:xs)
+			else
+				(x:(addUnique s xs))
+addUnique s [] = [s]
+
 
 runTrackerM :: TrackerM a -> IO a
 runTrackerM l = evalStateT l emptyEnv
